535f828431940d6dc310a76887868aace380323e
Aber <me@abersheeran.com>
2021-03-30 18:36:15 +0800

    Delete the performance warning because the performance loss is only about 1µs
    Just compare the following code in iPython
    
    ```python
    In [1]: %timeit sum(filter(lambda x: x % 2, range(1000)))
    109 µs ± 1.42 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
    
    In [2]: %timeit filter(lambda x: x % 2, range(1000)) | F(sum)
    110 µs ± 3.66 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
    
    In [3]: %timeit range(1000) | F(filter, lambda x: x % 2) | F(sum)
    109 µs ± 1.82 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
    ```

--- cool/pipe.py
+++ cool/pipe.py
@@ -4,9 +4,6 @@ from functools import partial
 class F(partial):
     """
     Python Pipe. e.g.`range(10) | F(filter, lambda x: x % 2) | F(sum)`
-
-    WARNING: There will be a small performance loss when building a
-    pipeline. Please do not use it in performance-sensitive locations.
     """
 
     def __ror__(self, other):
@@ -16,9 +13,6 @@ class F(partial):
 class FF(partial):
     """
     Python Pipe. e.g.`("f", 10) | FF(lambda letter, num: letter * num)`
-
-    WARNING: There will be a small performance loss when building a
-    pipeline. Please do not use it in performance-sensitive locations.
     """
 
     def __ror__(self, other):
